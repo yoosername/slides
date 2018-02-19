4e8aac6549fb0c42e34205ff3f9a9c840e35e7cc
yoosername <yoosername@local>
2018-02-19 23:00:05 +0000

    minor updates

--- app.js
+++ app.js
@@ -9,20 +9,6 @@ var port = 3000;
 app.use(express.static('public'));
 app.use('/preview', express.static(path.join(__dirname, './public/lib/reveal')));
 
-app.post('/reload', jsonParser, function(req, res) {
-
-  if (!req.body) return res.sendStatus(400);
-
-  fs.writeFile(__dirname + "/public/live.md", req.body.markdown, function(err) {
-      if(err) {
-          return res.sendStatus(400, err);
-      }
-      console.log("The slides were saved!");
-      res.end("success");
-  });
-
-});
-
 app.listen(port, function(error) {
   if (error) {
     console.error(error)
